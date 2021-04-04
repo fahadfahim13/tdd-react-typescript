b310d532f518df0bc8ab3fd465850c6ea3153cbc
fahadfahim13 <fahadfahim13@gmail.com>
2021-04-04 07:13:33 +0600

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// setup file
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+
+describe('Counter Testing', () => {
+  test('renders learn react link', () => {
+
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain('This is Counter App');
+
+    // render(<App />);
+    // const linkElement = screen.getByText('This is Counter App');
+    // expect(linkElement).toBeInTheDocument();
+  });
+  
+})
